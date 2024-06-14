0c5321e6ee1322fc75c43558ef53015acefdd846
Pisces12345 <104249923+Pisces12345@users.noreply.github.com>
2024-06-14 18:47:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,9 +18,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '昕昕要努力生活', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个考研人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notionproject-git-main-pisces12345.vercel.app/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '纯ocd, enfj, 计算机, 考研', // 网站关键词 英文逗号隔开
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个大学牲', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://pisce.buzz/', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '纯ocd, enfj, 计算机，cocos游戏开发，数学老师', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '2802484097@qq.com', // 邮箱地址 例如mail@tangly1024.com
